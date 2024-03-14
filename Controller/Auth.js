f731fc04dc6794c6f077a8ffa8f0d5cba71be1ec
Akash <73471799+AkashAkash311@users.noreply.github.com>
2024-03-14 10:20:01 +0530

    update Auth.js

--- Controller/Auth.js
+++ Controller/Auth.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const User = require("../Models/User");
-// const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken")
 
 require("dotenv").config()
 
@@ -61,9 +61,52 @@ exports.signup = async (req,res) =>{
 
 exports.login = async (req,res)=>{
     try{
+        const {email,password} = req.body;
 
-    }
-    catch(error){
+        if(!email || !password){
+            return res.status(400).json({
+                success : false,
+                message : "Please provide an email and a password"
+            })
+        }
+
+        //check for registered user
+        let user = await User.findOne( {email} );
+        if(!user){
+            return res.status(400).json({
+                success : false,
+                message : "User does not exist",
+            })
+        }
+
+        // Verify password & generate a JWT token
+
+        const payload = {
+            email : user.email,
+            id : user._id,
+            role : user.role,
+        };
         
+        if(await bcrypt.compare(password,user.password)){
+            //Password match
+            let token = jwt.sign(payload, process.env.JWT_SECRET,{
+                expiresIn : "2h",  //Token will expire in 2 hours
+            });
+
+            user = user.toObject();
+            user.token = token;
+            user.password = undefined;
+
+            const options = {
+                expies
+            }
+        }
+    }
+    catch(err){
+        console.error(err)
+        return res.status(500).json({
+            success : false,
+            message : "Login false" 
+        })
     }
 }
